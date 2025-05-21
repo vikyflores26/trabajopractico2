f1528fa8315f095e0f76fdc921e3a37f0625da89
alumno <alumno@Instituto.local>
2025-05-21 08:20:17 -0300

    cambio21/05

---
+++ script.js
@@ -0,0 +1,19 @@
+/*
+let alumnos = ["bauti","benja","facu"]
+for ( let indice = 0; indice < alumnos.length; inidce++){
+    console.log(alumnos[indice])
+}
+*/
+
+let frutas = ["frutilla","banana","mandarina"]
+console.log(frutas)
+
+frutas.push("manzana");
+console.log(frutas[frutas.length -1]);
+
+frutas.unshift("pera");
+console.log(frutas[0]);
+
+frutas.pop("manzana")
+console.log(frutas)
+
