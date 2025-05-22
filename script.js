030f2c44be8cc65a3f8e5597c6ca93d0278523b4
alumno <alumno@Instituto.local>
2025-05-22 11:21:35 -0300

    22/05

--- script.js
+++ script.js
@@ -14,6 +14,17 @@ console.log(frutas[frutas.length -1]);
 frutas.unshift("pera");
 console.log(frutas[0]);
 
-frutas.pop("manzana")
-console.log(frutas)
+frutas.pop("manzana");
+console.log(frutas);
 
+let nombres = ["clara","Mia","Victoria"];
+console.log(nombres.length);
+
+nombres.push("bauti");
+console.log (nombres);
+console.log(nombres,length);
+
+let numeros = ["1","2","3","4"];
+for (let numeros = 0; numeros < numeros.length; numeros 0 ++) {
+    console.log(numeros[numeros]);
+}
